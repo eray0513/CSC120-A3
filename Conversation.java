ca486a9e76f20e4c24ea07d47ade8c76619f0230
Evelyn Ray <eray@smith.edu>
2024-09-20 12:23:24 -0400

    wrote chat method

--- Conversation.java
+++ Conversation.java
@@ -4,7 +4,9 @@ class Conversation {
   // Attributes
   private int rounds;
   private String[] converse;
-  private int currentRound;
+
+  private String[] initialWords = {"I", "me", "am", "you", "my", "your",};
+  private String[] replaceWords = {"you", "you", "are", "I", "your", "my",};
 
   /**
    * Constructs a conversation object with a number of rounds and an array to store the strings
@@ -32,9 +34,31 @@ class Conversation {
     return this.converse;
   }
 
-  public String[] chat(String perRes){
-    
-      if(perRes.indexOf())
+  /**
+   * Allows the bot to formulate a response based on the user's input
+   * @param perRes String response of person
+   * @return respone of the bot
+   */
+  public String chat(String perRes){
+      String [] separated = perRes.split(" ");
+      String response = "";
+      // For each word
+      for(int i = 0; i<separated.length; i++){
+        // For each initial word
+        boolean rep = false;
+        for(int s = 0; s<initialWords.length; s++){
+            if(separated[i].equals(initialWords[s])){
+              response += " " + replaceWords[s];
+              rep = true;
+            }
+          }
+          if(rep){
+            response += " " + separated[i];
+          }
+        }
+      response += "?";
+      System.out.println(response);
+      return response;
   }
 
   public static void main(String[] arguments) {
@@ -50,5 +74,14 @@ class Conversation {
     String opener = "What are you thinking about today?";
     System.out.println(opener);
     current.converse[0] = opener;
+
+    for(int i = 1; i <=(totalRounds*2); i++){
+      String nextRes = input.next();
+      current.converse[i] = nextRes;
+      current.converse[i+1] = current.chat(nextRes);
+      i++;
+    }
+
+    System.out.println(current.getConverse());
   }
 }
