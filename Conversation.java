2907197098489428e3c31a0894b238c21eb4aba8
Evelyn Ray <eray@smith.edu>
2024-09-22 13:12:44 -0400

    more comments and math.random

--- Conversation.java
+++ Conversation.java
@@ -6,8 +6,8 @@ class Conversation {
   private String[] converse;
 
   // Lists of words to replace one another and canned responses
-  private String[] initialWords = {"I", "me", "am", "you", "my", "your", "I'm"};
-  private String[] replaceWords = {"you", "you", "are", "I", "your", "my", "you're"};
+  private String[] initialWords = {"I", "me", "am", "you", "my", "your", "I'm", "You", "Me", "My", "Your", "are"};
+  private String[] replaceWords = {"you", "you", "are", "I", "your", "my", "you're", "I", "You", "Your", "My", "am"};
   private String[] cannedWords = {"Interesting", "Mmmmhmmmm", "Wow"};
 
   /**
@@ -62,16 +62,19 @@ class Conversation {
         }
       // If no mirror words were found
       if(!mirror){
-        // Random canned response
-        response += cannedWords[(int)Math.random()*3];
+        // Random canned response\
+        int ranIndex = (int)Math.random()*3;
+        response += cannedWords[ranIndex];
       }
       else{
         // For every word in the separated list, add it to the response string
           for(int j = 0; j < separated.length; j++){
             response += separated[j] + " ";
         }
+        // returns the response with a question mark
         return response + "?";
       }
+      // if no mirror words return randomly generated canned response
       return response;
   }
 
@@ -80,6 +83,7 @@ class Conversation {
 
     Scanner input = new Scanner(System.in); // Creates a scanner object
 
+    // Requests input for number of rounds
     System.out.println("Hello! How many rounds would you like to have today?");
     int totalRounds = input.nextInt();
     input.nextLine(); // I'm not sure if this is the kind of thing you want me to disclose
@@ -87,25 +91,36 @@ class Conversation {
     // another line and the response reminded me that sometimes the scanner requires a line sort of reset
     // in order to allow it to take in the next line that the user implements
 
+    // Create a conversation object
     Conversation current = new Conversation(totalRounds);
 
+    // Starts the conversation and concatenates it to the transcript
     String opener = "What are you thinking about today?";
     System.out.println(opener);
     current.converse[0] = opener;
 
     // For every response given by the bot or the person
     for(int i = 1; i <(totalRounds*2); i++){
+      // Get the user input
       String nextRes = input.nextLine();
+      // Generate a response
       String botRes = current.chat(nextRes);
+      // Concatenate both
       current.converse[i] = nextRes;
       current.converse[i+1] = botRes;
+      // Print response
       System.out.println(botRes);
+      // Skip over the index that the bots response was concatenated to
       i++;
     }
+    // Print and concatenate farewell
     System.out.println("Goodbye!");
     current.converse[current.converse.length-1] = "Goodbye!";
+    
+    // Leave space
     System.out.println(" ");
     System.out.println(" ");
+    // Print transcript
     System.out.println(current.getConverse());
   }
 }
